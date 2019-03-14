5c71e7821f3e354db8c594fd612df35b11d30483
Skyrimus <Skyrimus@yandex.ru>
2019-03-14 21:05:26 +0200

    RR Patches

---
+++ patches_mtk/apply-patches-rr.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+cd ../../../..
+cd system/core
+git apply -v ../../device/wileyfox/porridge/patches_mtk/system_core.patch
+cd ../..
+cd bionic
+git apply -v ../device/wileyfox/porridge/patches_mtk/bionic.patch
+cd ..
+cd system/sepolicy
+git apply -v ../../device/wileyfox/porridge/patches_mtk/system_sepolicy_rr.patch
+cd ../..
+cd packages/apps/Settings
+git apply -v ../../../device/wileyfox/porridge/patches_mtk/settings.patch
+cd ../../..
+cd frameworks/av
+patch -p1 < ../../device/wileyfox/porridge/patches_mtk/frameworks_av_new.patch
+cd ..
+cd native
+patch -p1 < ../../device/wileyfox/porridge/patches_mtk/frameworks_native_new.patch
+cd ..
+cd base
+git apply -v ../../device/wileyfox/porridge/patches_mtk/frameworks_base.patch
+cd ..
+cd opt/telephony
+git apply -v ../../../device/wileyfox/porridge/patches_mtk/frameworks_opt_telephony.patch
+cd ../../..
+cd packages/apps/Snap
+git apply -v ../../../device/wileyfox/porridge/patches_mtk/snap.patch
+cd ..
+cd FMRadio
+git apply -v ../../../device/wileyfox/porridge/patches_mtk/fmradio.patch
+cd ../../..
+cd system/netd
+git apply -v ../../device/wileyfox/porridge/patches_mtk/system_netd.patch
+cd ../..
+cd vendor/cmsdk
+git apply -v ../../device/wileyfox/porridge/patches_mtk/vendor_cmsdk.patch
+cd ../..
+
